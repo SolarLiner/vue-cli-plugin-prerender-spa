0dab81c293f94ea5528891de9ac5d8c27bdee59f
snadn <snadn@snadn.cn>
2018-10-11 18:15:54 +0800

    Support pages options (#11)
    Fixes #10

--- index.js
+++ index.js
@@ -22,33 +22,57 @@ module.exports = (api, projectOptions) => {
     if (options.customRendererConfig) {
       Object.assign(rendererConfig, options.customRendererConfig);
     }
-    
+
     const renderer = new Renderer(rendererConfig);
-    if (projectOptions.baseUrl) {
-      renderer.preServer = (Prerenderer) => {
+    renderer.preServer = (Prerenderer) => {
+      if (projectOptions.baseUrl) {
         const prefix = projectOptions.baseUrl;
-        Prerenderer._server._expressServer.use((req, res, next) => {
+        const server = Prerenderer._server._expressServer;
+        server.use((req, res, next) => {
           if (req.url.indexOf(prefix) === 0) {
             req.url = req.url.slice(prefix.length - 1);
           }
           next();
         });
-      };
+      }
+      if (projectOptions.pages) {
+        const server = Prerenderer._server._expressServer;
+        server.get('*', (req, res, next) => {
+          if (!path.extname(req.url)) {
+            const filePath = api.resolve(`${projectOptions.outputDir}${req.url}${path.basename(req.url) ? '.html' : 'index.html'}`);
+            fs.exists(filePath, (exists) => exists ? res.sendFile(filePath) : next());
+            return;
+          }
+          next();
+        })
+      }
+    };
+
+    const outputDir = api.resolve(projectOptions.outputDir);
+    const staticDir = api.resolve(projectOptions.outputDir);
+    const prerenderOptions = {
+      outputDir,
+      staticDir,
+      assetsDir: api.resolve(
+        path.join(projectOptions.outputDir, projectOptions.assetsDir)
+      ),
+      indexPath: api.resolve(
+        path.join(projectOptions.outputDir, projectOptions.indexPath)
+      ),
+      routes: options.renderRoutes,
+      renderer
+    };
+
+    prerenderOptions.postProcess = (renderedRoute) => {
+      const route = renderedRoute.route;
+      if (route[route.length - 1] !== '/' && path.extname(route) === '') {
+        renderedRoute.outputPath = path.join(outputDir || staticDir, `${route}.html`)
+      }
+      return renderedRoute;
     }
 
     config.plugin("pre-render").use(PrerenderSPAPlugin, [
-      {
-        outputDir: api.resolve(projectOptions.outputDir),
-        staticDir: api.resolve(projectOptions.outputDir),
-        assetsDir: api.resolve(
-          path.join(projectOptions.outputDir, projectOptions.assetsDir)
-        ),
-        indexPath: api.resolve(
-          path.join(projectOptions.outputDir, projectOptions.indexPath)
-        ),
-        routes: options.renderRoutes,
-        renderer
-      }
+      prerenderOptions
     ]);
   });
 };
